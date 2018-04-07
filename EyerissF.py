0732e7d4345e70e6efa5d7781b5110d9db0584d4
frony <pm.ccc@hotmail.com>
2018-04-07 22:52:33 +0800

    finish FilterReuse

--- EyerissF.py
+++ EyerissF.py
@@ -37,21 +37,41 @@ class EyerissF:
         pass
 
     def FmapReuse(self,Pictures,FilterWights):
+        line = list()
+        l = list()
+        Picture = Pictures
+        for x in range(0, len(FilterWights[0])):
+            # third, move the next pic line
+            for y in range(0, len(FilterWights[0][0])):
+                # second, in the same line do the same operations
+                for z in range(0, len(FilterWights)):
+                    # first, write different pics' pixs in a line
+                    l.append(FilterWights[z][x][y])
+            line.append(np.hstack(l))
+            l.clear()
+        FilterWight = np.array(line)
+        return Picture, FilterWight
+
+    def FilterReuse(self,Pictures,FilterWights):
+        FilterWight=FilterWights
         l = list()
         line = list()
         for y in range(0, len(Pictures[0])):
+            # second move to next line to loop the inner operations
             for x in range(0, len(Pictures)):
+                # first combine two lines ,which locate on different pics, to one line
                 l.append(Pictures[x][y])
             line.append(np.hstack(l))
             l.clear()
         Picture = np.array(line)
-        return Picture,FilterWights
-
-    def FilterReuse(self,Pictures,FilterWights):
-        pass
+        return Picture, FilterWight
 
     def ChannelAccumulation(self,Pictures,FilterWights):
         pass
 
     def run(self):
+        pass
+
+    def RawStationry(self,Pictures,FilterWights):
+        # Call Two Reuse function and ChannelAccumulation
         pass
--- NumpyTest.py
+++ NumpyTest.py
@@ -5,16 +5,3 @@ Pic2=np.array([[9,10],[11,12],[13,14],[15,16]])
 Pic=list()
 Pic.append(Pic1)
 Pic.append(Pic2)
-
-def FmapReuse(Pictures,FilterWights=0):
-    length=len(Pictures)
-    l = list()
-    line=list()
-    for y in range(0, len(Pictures[0])):
-        for x in range(0, length):
-            l.append(Pictures[x][y])
-        line.append(np.hstack(l))
-        l.clear()
-    c=np.array(line)
-    return c
-
