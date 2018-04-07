5e22213625a7b6a69ad53f43600f95309dd4e344
frony <pm.ccc@hotmail.com>
2018-04-08 02:17:03 +0800

    waiting for tranform Psum

--- EyerissF.py
+++ EyerissF.py
@@ -12,11 +12,14 @@ class EyerissF:
     def __init__(self):
         pass
 
-    def Conv2d(self,Pictures,FilterWights,Activation='Relu'):
-        self.RawStationry(Pictures,FilterWights)
+    def Conv2d(self,Pictures,FilterWeights,Activation='Relu'):
+        self.RawStationry(Pictures,FilterWeights)
         self.DataDeliver()
         self.run()
+
+        # TODO add paramter in ()
         eval('Activiation.' + Activation + '()')
+
 
         ###########################################
         # if Activiation=='Relu' :
@@ -32,28 +35,57 @@ class EyerissF:
             for y in range(0,conf.EyerissWidth):
                 self.PEArray[x].append(PE())
 
-    def DataDeliver(self):
-        #将一个pic和filter分发到eyeirss中
-        pass
 
-    def FmapReuse(self,Pictures,FilterWights):
+    def DataDeliver(self,Picture,FilterWeight):
+        #将一个pic和filter分发到eyeriss中
+
+        #防止越界
+        assert len(FilterWeight)<=self.EyerissHeight
+        assert len(Picture)+len(Picture[0])-1 <= self.EyerissWidth
+
+        # filterWeight input from left to right
+        for x in range(0,len(FilterWeight)):
+            for y in range(0,self.EyerissWidth):
+                self.PEArray[x][y].setFilterWeight(FilterWeight[x])
+
+        # ImageRow input from left-down to righ-up
+
+        for z in range(0,len(Picture)):
+            x = 0
+            y = z
+            for c in range(0,z+1):
+                self.PEArray[y][x].setImageRow(Picture[z])
+                x=x+1
+                y=y-1
+
+    def FmapReuse(self,Pictures,FilterWeights):
+
+        assert len(Pictures)==1
+
+        #TODO Rename l and line
+
         line = list()
         l = list()
         Picture = Pictures
-        for x in range(0, len(FilterWights[0])):
+        for x in range(0, len(FilterWeights[0])):
             # third, move the next pic line
-            for y in range(0, len(FilterWights[0][0])):
+            for y in range(0, len(FilterWeights[0][0])):
                 # second, in the same line do the same operations
-                for z in range(0, len(FilterWights)):
+                for z in range(0, len(FilterWeights)):
                     # first, write different pics' pixs in a line
-                    l.append(FilterWights[z][x][y])
+                    l.append(FilterWeights[z][x][y])
             line.append(np.hstack(l))
             l.clear()
-        FilterWight = np.array(line)
-        return Picture, FilterWight
+        FilterWeight = np.array(line)
+        return Picture, FilterWeight
 
-    def FilterReuse(self,Pictures,FilterWights):
-        FilterWight=FilterWights
+    def FilterReuse(self,Pictures,FilterWeights):
+
+        assert len(FilterWeights)==1
+
+        # TODO Rename l and line
+
+        FilterWeight=FilterWeights
         l = list()
         line = list()
         for y in range(0, len(Pictures[0])):
@@ -64,14 +96,46 @@ class EyerissF:
             line.append(np.hstack(l))
             l.clear()
         Picture = np.array(line)
-        return Picture, FilterWight
+        return Picture, FilterWeight
 
-    def ChannelAccumulation(self,Pictures,FilterWights):
+    def ChannelAccumulation(self,Pictures,FilterWeights):
         pass
 
     def run(self):
-        pass
+        for x in range(0,conf.EyerissHeight):
+            for y in range(0,conf.EyerissWidth):
+                self.PEArray[x][y].countPsum()
 
-    def RawStationry(self,Pictures,FilterWights):
+
+    def RawStationry(self,Pictures,FilterWeights):
         # Call Two Reuse function and ChannelAccumulation
         pass
+
+
+if __name__=='__main__':
+    Pic1 = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    Pic2 = np.array([[9, 10], [11, 12], [13, 14], [15, 16]])
+
+    e=EyerissF()
+    e.InitPEs()
+
+
+    e.DataDeliver(Pic1,Pic2)
+    print(e.PEArray[0][0].ImageRow)
+    print(e.PEArray[1][0].ImageRow)
+    print(e.PEArray[0][1].ImageRow)
+
+    print(e.PEArray[2][0].ImageRow)
+    print(e.PEArray[1][1].ImageRow)
+    print(e.PEArray[0][2].ImageRow)
+    e.run()
+
+
+
+    print(e.PEArray[0][0].Psum)
+    print(e.PEArray[1][0].Psum)
+    print(e.PEArray[0][1].Psum)
+
+    print(e.PEArray[2][0].Psum)
+    print(e.PEArray[1][1].Psum)
+    print(e.PEArray[0][2].Psum)
--- PE.py
+++ PE.py
@@ -6,5 +6,35 @@ class PE:
     PEBuffer=conf.PEBuffer
 
     def __init__(self):
-        pass
+        self.setFilterWeight((0,0))
+        self.setImageRow((0,0))
 
+    def setFilterWeight(self,FilterWeight):
+        self.FilterWeight=FilterWeight
+
+    def setImageRow(self, ImageRow):
+        self.ImageRow = ImageRow
+
+    def Conv1d(self,ImageRow,FilterWeight):
+        result = list()
+        for x in range(0,len(ImageRow)-1+len(FilterWeight)):
+            y=x+len(FilterWeight)
+            if y>len(ImageRow):
+                break
+            r=ImageRow[x:y]*FilterWeight
+            result.append(r.sum())
+        return np.array(result)
+
+    def countPsum(self):
+
+        try:
+            self.Psum=self.Conv1d(self.FilterWeight,self.ImageRow)
+        except:
+            pass
+
+
+
+if __name__=='__main__':
+    p=PE()
+    r=p.Conv1d(np.array([1,1,1,1,1]),np.array([1,1]))
+    print(r)
