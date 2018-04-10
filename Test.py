7cc1af3cc3f0509819bfbf01f229b0d09f234688
frony <pm.ccc@hotmail.com>
2018-04-11 02:43:10 +0800

    first usable edition with pic-filter-turn bug fixed

--- Test.py
+++ Test.py
@@ -3,11 +3,13 @@ import numpy as np
 import time
 
 a=time.time()
+
 ef=EF()
 ef.InitPEs()
 p=np.array([[4,3,1,0],[2,1,0,1],[1,2,4,1],[3,1,0,2]])
 f=np.array([[1,0,1],[2,1,0],[0,0,1]])
 r=ef.Conv2d(p,f)
+
 b=time.time()
 
 print(p)
