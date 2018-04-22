a7516a0cc420166baabe01adccee395e4b808d02
frony <pm.ccc@hotmail.com>
2018-04-22 14:03:30 +0800

    finish compress and decompress

--- IOCompression.py
+++ IOCompression.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def Compress(NpArray):
-
     Row, Column = NpArray.shape
     ComedNpArray = np.array([Row, Column], dtype=int)
     NpArray = NpArray.reshape((1, Row * Column))
@@ -24,9 +23,36 @@ def Compress(NpArray):
 
     return ComedNpArray
 
-def Decompress():
-    pass
 
-if __name__=="__main__":
-    NpArray = np.array([[0,0,0,1,1,1,0,0],[1,1,0,0,0,0,0,0],[1,1,0,0,0,0,0,0],[1,1,0,0,0,0,0,0]])
-    print(Compress(NpArray))
+def Decompress(NpArray):
+    Length = NpArray.size - 2
+    Row = NpArray[0]
+    Column = NpArray[1]
+
+    DecomedNpArray = list()
+
+    for interr in range(Length):
+
+        if NpArray[interr + 2] == 0:
+            for x in range(NpArray[interr + 3]):
+                DecomedNpArray.append(0)
+
+        elif NpArray[interr + 1] == 0:  # NpArray[interr+2] != 0
+            pass
+
+        else:  # NpArray[interr+2] != 0 && NpArray[interr+1] != 0
+            DecomedNpArray.append(NpArray[interr + 2])
+
+    DecomedNpArray = np.array(DecomedNpArray)
+    DecomedNpArray = DecomedNpArray.reshape(Row, Column)
+    return DecomedNpArray
+
+
+if __name__ == "__main__":
+    NpArray = np.array(
+        [[0, 0, 0, 1, 1, 1, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0]])
+    print(NpArray)
+    c = Compress(NpArray)
+    print(c)
+    c = Decompress(c)
+    print(c)
