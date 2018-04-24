2d7ae1ca0b233036fcc19b8bcce166b97111a4d7
frony <pm.ccc@hotmail.com>
2018-04-24 22:47:59 +0800

    delete alexnet.py

--- LeNet5.py
+++ LeNet5.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+
+ConvLayer1Filter1=np.load('ConvLayerFilter/ConvLayer1Filter1.npy')
+ConvLayer1Filter2=np.load('ConvLayerFilter/ConvLayer1Filter2.npy')
+ConvLayer1Filter3=np.load('ConvLayerFilter/ConvLayer1Filter3.npy')
+ConvLayer1Filter4=np.load('ConvLayerFilter/ConvLayer1Filter4.npy')
+ConvLayer1Filter5=np.load('ConvLayerFilter/ConvLayer1Filter5.npy')
+ConvLayer1Filter6=np.load('ConvLayerFilter/ConvLayer1Filter6.npy')
